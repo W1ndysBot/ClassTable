56b15fed4577cad9c576b8135993fa4bfa5f0ba7
W1ndys <w1ndys@outlook.com>
2024-09-05 11:23:54 +0800

    init

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Example
+# ClassTable
 
-W1ndys-bot 的功能模块编写示例文件
+通过 wakeupAPI 导入课表实现定时上课提醒
--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-# script/example/main.py
-# 示例脚本
-# 本脚本写好了基本的函数，直接在函数中编写逻辑即可，必要的时候可以修改函数名
-# 注意：Example 是具体功能，请根据实际情况一键替换即可
-# 注意：function 是函数名称，请根据实际情况一键替换即可
+# script/ClassTable/main.py
+
 
 import logging
 import os
@@ -18,11 +15,11 @@ from app.config import owner_id
 from app.api import *
 from app.switch import load_switch, save_switch
 
-# 数据存储路径，实际开发时，请将Example替换为具体的数据存放路径
+# 数据存储路径，实际开发时，请将ClassTable替换为具体的数据存放路径
 DATA_DIR = os.path.join(
     os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))),
     "data",
-    "Example",
+    "ClassTable",
 )
 
 
@@ -37,49 +34,17 @@ def save_function_status(group_id, status):
 
 
 # 群消息处理函数
-async def handle_Example_group_message(websocket, msg):
-    try:
-        user_id = str(msg.get("user_id"))
-        group_id = str(msg.get("group_id"))
-        raw_message = str(msg.get("raw_message"))
-        role = str(msg.get("sender", {}).get("role"))
-        message_id = str(msg.get("message_id"))
-
-    except Exception as e:
-        logging.error(f"处理Example群消息失败: {e}")
-        return
-
-
-# 群通知处理函数
-async def handle_Example_group_notice(websocket, msg):
-    try:
-        user_id = str(msg.get("user_id"))
-        group_id = str(msg.get("group_id"))
-        raw_message = str(msg.get("raw_message"))
-        role = str(msg.get("sender", {}).get("role"))
-        message_id = str(msg.get("message_id"))
-
-    except Exception as e:
-        logging.error(f"处理Example群通知失败: {e}")
-        return
-
-
-# 私聊消息处理函数
-async def handle_Example_private_message(websocket, msg):
-    try:
-        user_id = str(msg.get("user_id"))
-        raw_message = str(msg.get("raw_message"))
-
-    except Exception as e:
-        logging.error(f"处理xxx私聊消息失败: {e}")
-        return
-
-
-async def Example_main(websocket, msg):
-
+async def handle_ClassTable_group_message(websocket, msg):
     # 确保数据目录存在
     os.makedirs(DATA_DIR, exist_ok=True)
 
-    await handle_Example_group_message(websocket, msg)
-    await handle_Example_group_notice(websocket, msg)
-    await handle_Example_private_message(websocket, msg)
+    try:
+        user_id = str(msg.get("user_id"))
+        group_id = str(msg.get("group_id"))
+        raw_message = str(msg.get("raw_message"))
+        role = str(msg.get("sender", {}).get("role"))
+        message_id = str(msg.get("message_id"))
+
+    except Exception as e:
+        logging.error(f"处理ClassTable群消息失败: {e}")
+        return
